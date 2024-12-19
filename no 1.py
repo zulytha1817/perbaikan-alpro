30c4ec4cf9eedca86478438bf94b7c6eadbf21ff
zulytha1817 <zulythadwis@gmail.com>
2024-12-19 15:24:15 +0700

    first commit

---
+++ no 1.py
@@ -0,0 +1,5 @@
+n = int(input("masukkan nilai n : "))
+total = 0
+for i in range(1,n+1):
+    total += i
+print(f"Jumlah bilangan daari 1 hingga {n} adalah {total}")
---
+++ no 2.py
@@ -0,0 +1,3 @@
+r = float(input("masukkan jari-jari : "))
+luas = 3.14 * pow(r,2)
+print("luas lingkaran = ",luas)
